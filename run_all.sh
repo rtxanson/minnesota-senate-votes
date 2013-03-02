e6e1da4740ed0c783033a26b82e7b6abbbd016bd
Ryan <ryan.txanson@gmail.com>
2013-03-02 01:39:49 -0600

    for your convenience

---
+++ run_all.sh
@@ -0,0 +1,9 @@
+﻿rm last_run_status.txt
+
+ls -c1 2008_2009/sessions/txt/*.txt | xargs -I {} python record_parser.py read {} {}.json --with-amendments
+ls -c1 2009_2010/sessions/txt/*.txt | xargs -I {} python record_parser.py read {} {}.json --with-amendments
+ls -c1 2011_2012/sessions/txt/*.txt | xargs -I {} python record_parser.py read {} {}.json --with-amendments
+
+mv 2008_2009/sessions/txt/*.txt.json 2008_2009/sessions/json/
+mv 2009_2010/sessions/txt/*.txt.json 2009_2010/sessions/json/
+mv 2011_2012/sessions/txt/*.txt.json 2011_2012/sessions/json/
